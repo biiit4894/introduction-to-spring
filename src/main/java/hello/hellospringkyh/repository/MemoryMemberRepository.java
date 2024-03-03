b0b1a985c85bd691ae4970f13595188d8c72aade
Hanbit <ghiesf@naver.com>
2024-03-03 23:59:53 +0900

    ㅁAdd: 회원관리 예제 - 백엔드 개발

---
+++ src/main/java/hello/hellospringkyh/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package hello.hellospringkyh.repository;
+
+import hello.hellospringkyh.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));// 매개변수가 null이어도 감쌀 수 있게 됨
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
