a4935eecb3c6160b04006d97c8f9b837d6c7636d
Hanbit <ghiesf@naver.com>
2024-03-09 14:21:21 +0900

    Add: Spring Data JPA 적용

---
+++ src/main/java/hello/hellospringkyh/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,11 @@
+package hello.hellospringkyh.repository;
+
+import hello.hellospringkyh.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    @Override
+    Optional<Member> findByName(String name);
+}
